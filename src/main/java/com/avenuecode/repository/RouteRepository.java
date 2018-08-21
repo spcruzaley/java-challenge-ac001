2b259ee42bfb50bc304fbb087f14b9c76dbbddc2
salvador.perez <salvador.perez@globant.com>
2018-08-21 12:04:51 -0500

    First commit to add the proyect to a CI process

---
+++ src/main/java/com/avenuecode/repository/RouteRepository.java
@@ -0,0 +1,12 @@
+package com.avenuecode.repository;
+
+import com.avenuecode.domain.Route;
+import com.avenuecode.domain.Town;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface RouteRepository  {
+    
+}
