91699f4f79a95f26bc399fe1d49157ebc870e6b5
salvador.perez <salvador.perez@globant.com>
2018-08-23 16:15:15 -0500

    Added configurations for CI

--- src/main/java/com/avenuecode/Application.java
+++ src/main/java/com/avenuecode/Application.java
@@ -3,8 +3,11 @@ package com.avenuecode;
 import com.avenuecode.repository.RouteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
+@EnableAutoConfiguration(exclude = HibernateJpaAutoConfiguration.class)
 @SpringBootApplication
 public class Application {
 
